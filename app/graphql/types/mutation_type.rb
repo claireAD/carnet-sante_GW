34995cf39dba74ea049c0b99901c9726bc4e594b
Claire Dufetrelle <claire.dufetrelle@fabernovel.com>
2018-02-14 09:06:33 +0100

    Add sign in method

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,4 +3,5 @@ Types::MutationType = GraphQL::ObjectType.define do
 
   field :createAllergy, function: Resolvers::CreateAllergy.new
   field :createUser, function: Resolvers::CreateUser.new
+  field :signinUser, function: Resolvers::SignInUser.new
 end
