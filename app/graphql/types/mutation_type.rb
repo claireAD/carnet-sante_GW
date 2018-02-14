ebf64a8ca7671002dcc0814dd4f58680e9084567
Claire Dufetrelle <claire.dufetrelle@fabernovel.com>
2018-02-14 08:59:41 +0100

    Add mutation to create user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,4 +2,5 @@ Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   field :createAllergy, function: Resolvers::CreateAllergy.new
+  field :createUser, function: Resolvers::CreateUser.new
 end
